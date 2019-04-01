32d0a281bfbd3c28961df4cc997b36e9fe0758e3
grayp <gray.pritchett@optusnet.com.au>
2019-04-01 11:53:09 +1000

    Add additonal projects

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("codeflagz")]
+[assembly: AssemblyProduct("CogRacer")]
+[assembly: AssemblyCopyright("Copyright reserved")]
+[assembly: AssemblyTrademark("Code Flag Z inc")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
